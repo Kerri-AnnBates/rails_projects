f580f9e56fb8242e41b5917736278d0f324b99c5
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2025-05-04 12:10:50 -0400

    set up comments controller

--- comments-api/app/controllers/comments_controller.rb
+++ comments-api/app/controllers/comments_controller.rb
@@ -1,5 +1,44 @@
 class CommentsController < ApplicationController
+  before_action :verify_comment, only: [:show, :update, :destroy]
+
   def index
     respond_with(Comment.all)
+  end
+
+  def create
+    @comment = Comment.create(comment_params)
+
+    if @comment.save
+      respond_with(@comment)
+    else
+      respond_with_bad_request
+    end
+  rescue => e
+    puts "Error: #{e.class} - #{e.message}"
+  end
+
+  def show
+    respond_with(@comment)
+  end
+
+  def update
+    @comment.update(comment_params)
+  end
+
+  def destroy
+    @comment.destroy
+    respond_no_content
+  end
+
+  private
+  def verify_comment
+    @comment = Comment.find([params[:id]])
+    respond_not_found("Comment with id #{params[:id]} not found!") unless @comment
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content, :account_id)
+  rescue => error
+    puts error
   end
 end
--- comments-api/config/routes.rb
+++ comments-api/config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :accounts, only: [:index, :show] do
-    resources :comments, only: [:index, :create]
-  end
+  resources :accounts, only: [:index, :show]
 
   resources :comments, only: [:index, :show, :create, :update, :destroy] do
     resources :replies, only: [:index, :show, :create, :update, :destroy], controller: 'comments'
