1d186fb6aefdc7bcafbe40f329b3bfb0e556917d
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2025-07-14 21:26:53 -0400

    test getting individual account

--- comments-api/spec/requests/account_spec.rb
+++ comments-api/spec/requests/account_spec.rb
@@ -11,4 +11,15 @@ RSpec.describe 'Account Api', type: :request do
       expect(json.size).to eq(3)
     end
   end
+
+  describe 'GET /account/{id}' do
+    it 'returns a success response: 200' do
+      user = create(:account)
+
+      get "/accounts/#{user.id}"
+      expect(response).to have_http_status(:ok)
+      json = JSON.parse(response.body)
+      expect(json["username"]).to eq(user.username)
+    end
+  end
 end
