b292a1da81ce183db1e3ad6adc82ca1ab5e044f7
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2025-12-14 20:40:17 -0500

    refactor

--- comments-api/app/controllers/auth_controller.rb
+++ comments-api/app/controllers/auth_controller.rb
@@ -6,10 +6,15 @@ class AuthController < ApplicationController
 
     if account && account.authenticate(params[:password])
       payload = { account_id: account.id, exp: 24.hours.from_now.to_i }
-      token = JWT.encode(payload, Rails.application.secret_key_base)
+      token = encode_token(payload)
       render json: { token: token, account: account.as_json(only: [ :id, :username ]) }, status: :ok
     else
       respond_with_unauthorized("Invalid username or password")
     end
+  end
+
+  private
+  def encode_token(payload)
+    JWT.encode(payload, Rails.application.secret_key_base)
   end
 end
